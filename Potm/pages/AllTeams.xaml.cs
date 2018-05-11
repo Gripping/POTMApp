2ce6b081ea1ac3b1e55da083a586b499cb766f11
ande4559 <ande4559@edu.eal.dk>
2018-05-11 14:27:55 +0200

    allteams styling and binding context from api calls coded

--- Potm/pages/AllTeams.xaml.cs
+++ Potm/pages/AllTeams.xaml.cs
@@ -5,13 +5,15 @@ using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using System.Diagnostics;
 using Potm.Data;
-
+using DLToolkit.Forms.Controls;
+using DLToolkit.Forms;
 namespace Potm.pages
 {
     public partial class AllTeams : ContentPage
     {
 		int sportId;
         readonly CollectionManager manager = new CollectionManager();
+		public FlowObservableCollection<club> flowClubs = new FlowObservableCollection<club>();
 
         public AllTeams(int sId)
         {
@@ -24,6 +26,19 @@ namespace Potm.pages
 		protected override async void OnAppearing()
 		{
 			var filteredClubs = await manager.GetFilteredClubs(sportId);
+
+			foreach (var c in filteredClubs )
+			{
+				flowClubs.Add(c);
+			}
+
+			clubList.FlowItemsSource = flowClubs;
+				
 		}
+
+		public async void FlowItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await Navigation.PushAsync(new SingleTeam((teams)e.Item));
+        }
     }
 }
